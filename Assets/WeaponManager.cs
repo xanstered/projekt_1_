728e48df1cbbcab15e56f88b0b520d2153989a68
xanstered <gzak2@edu.cdv.pl>
2024-03-14 10:35:47 +0100

    dalej

--- Assets/WeaponManager.cs
+++ Assets/WeaponManager.cs
@@ -6,9 +6,14 @@ public class WeaponManager : MonoBehaviour
 {
     public Weapon weapon1;
     public Weapon weapon2;
+    public Weapon weapon3;
 
-    public WeaponRaw weaponRaw1 = new WeaponRaw();
-    public WeaponRaw weaponRaw2 = new WeaponRaw();
+    // public WeaponRaw weaponRaw1 = new WeaponRaw(); //konstruktor czyli metoda kazdej klasy ktorego nie trzeba pisac bo automatycznie jest i ukryte jest 
+    // public WeaponRaw weaponRaw2 = new WeaponRaw();
+
+    public WeaponRaw weaponRaw1 = new WeaponRaw("AK47", 45);
+    public WeaponRaw weaponRaw2 = new WeaponRaw("Luger", 8, true); //latwo tworzymy nowe obiekty dzieki konstruktorowi
+    public WeaponRaw weaponRaw3 = new WeaponRaw("Sword", 1, Rarity.Legendary);
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +21,20 @@ public class WeaponManager : MonoBehaviour
         Debug.Log(weapon1.weaponName + weapon1.ammo);
         Debug.Log(weapon2.weaponName + weapon2.ammo);
 
-        weaponRaw1.SetInfo("Luger", 8);
-        weaponRaw2.SetInfo("AK47", 45, true);
+        // weaponRaw1.SetInfo("Luger", 8);
+        // weaponRaw2.SetInfo("AK47", 45, true);
 
         Debug.Log(weaponRaw1.weaponName + weaponRaw1.ammo);
-        Debug.Log(weaponRaw2.weaponName + weaponRaw2.ammo);
+        Debug.Log(weaponRaw2.weaponName + weaponRaw2.ammo = weaponRaw2.isBoost);
+        Debug.Log(weaponRaw3.weaponName + weaponRaw3.ammo = weaponRaw2.weaponRarity);
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum Rarity // po prostu nazwane liczby i tyle i enum jest poza klas¹ i s¹ odrêbne dziêki czemu dzia³aj¹ do wszystkiego i nie trzeba przepisywaæ ich kilka razy
     {
-        
+        Common = 1,
+        Uncommon,
+        Rare,
+        Super_Rare,
+        Legendary
     }
 }
--- Assets/WeaponRaw.cs
+++ Assets/WeaponRaw.cs
@@ -6,17 +6,29 @@ public class WeaponRaw
 {
     public string weaponName;
     public int ammo;
+    public bool isBoost;
+    public Rarity weaponRarity;
 
-    public void SetInfo(string p_name, int p_ammo)
+    public WeaponRaw(string p_name, int p_ammo)
     {
-        weaponName = p_name;
+        weaponName = p_name;    
         ammo = p_ammo;
     }
 
-    public void SetInfo(string p_name, int p_ammo, bool p_isBoost) //klucz wystêpowania, przeci¹¿anie metod, do parametrow EWNTUALNYCH  
+    public WeaponRaw(string p_name, int p_ammo, bool p_isBoost)
     {
         weaponName = p_name;
         ammo = p_ammo;
+        isBoost = p_isBoost;
     }
+    
+    public WeaponRaw(string p_name, int p_ammo, Rarity p_rarity)
+    {
+        weaponName = p_name;
+        ammo = p_ammo * (int)p_rarity; //int wylicza enum
+        weaponRarity = p_rarity;
+    }
+
+    // public void SetInfo(string p_name, int p_ammo, bool p_isBoost) to klucz wystêpowania, przeci¹¿anie metod, do parametrow EWNTUALNYCH  
 
 }
